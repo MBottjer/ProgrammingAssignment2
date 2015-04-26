95f7aca7f34b7b1fb10194e68c8721bc6494f40d
Michael Bottjer <michaelbottjer@hotmail.com>
2015-04-26 20:04:49 +0100

    add comments for assessment

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,23 @@
 
 ## Write a short comment describing this function
 
+# As a general overview, makeCacheMatrix is a function with a subset of functions. These functions are set, get,
+# setinverse and getinverse.
+
+# The function begins by initialising the variable inverse_of_matrix, which is initially set to NULL.
+
+# The first function, 'set', sets the value of the matrix when a value is passed to the following: 
+# makeCachMatrix$set(valuePassedhere). This overides the currently stored value of x in 
+# makeCacheMatrix, hence the use of <<-, and resets the previously calculated inverse to NULL.
+
+# 'get' simply returns the currently value of x.
+
+# 'setinverse' merely takes a parameter and assigns the variable 'inverse_of_matrix' to that value.
+
+# 'get_inverse' returns the current inverse of the stored matrix of inverse_of_matrix
+
+# at the end fo the function we return a list of functions so that we have access to them
+
 makeCacheMatrix <- function(x = matrix()) {
 
   inverse_of_matrix <- NULL
